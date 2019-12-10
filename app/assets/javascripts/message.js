83e72510d601c813e136a6c412dd3326c23ad6de
MasashiTomita <sagittariusblue1212@gmail.com>
2019-12-10 18:48:39 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,6 @@
 $(function(){
   function buildHTML(message){
+    
     if (message.image) {
       var html = 
         `<div class="main-chat__contents__box" data-message-id="${message.id}">
@@ -43,24 +44,50 @@ $(function(){
     e.preventDefault()
     var formData = new FormData(this);
     var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: 'POST',  
-      data: formData,  
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.main-chat__contents').append(html);
-      $('.form__submit').prop('disabled', false);
-      $('.main-chat__contents').animate({scrollTop: $('.main-chat__contents')[0].scrollHeight},'fast');
-      $('form')[0].reset();
-    })
-    .fail(function(){
-      alert('error');
-    })
-    return false;
+      last_message_id = $('.main-chat__contents__box').last().data('message-id');
+      $.ajax({
+        url: url,
+        type: 'POST',  
+        data: formData,  
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.main-chat__contents').append(html);
+        $('.form__submit').prop('disabled', false);
+        $('.main-chat__contents').animate({scrollTop: $('.main-chat__contents')[0].scrollHeight},'fast');
+        $('form')[0].reset();
+      })
+      .fail(function(){
+        alert('error');
+      })
+     return false;
   })
-})
+  
+  
+  var reloadMessages = function () {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      last_message_id = $('.main-chat__contents__box').last().data('message-id');
+      $.ajax({
+        url: "api/messages",
+        type: 'get',  
+        data: {id: last_message_id},  
+        dataType: 'json',
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main-chat__contents').append(insertHTML);
+        $('.main-chat__contents').animate({scrollTop: $('.main-chat__contents')[0].scrollHeight},'fast');
+      })
+      .fail(function() {
+        alert('error');
+      });
+    };
+  };
+  setInterval(reloadMessages, 3000);
+});
